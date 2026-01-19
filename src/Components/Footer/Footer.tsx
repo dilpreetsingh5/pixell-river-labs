ae1dbb57b8cc2e8892894a9f803cb17947ddc883
dilpreetsingh5 <dilpreetsingh5@example.com>
2026-01-19 01:32:43 -0600

    Create Footer component with dynamic copyright year

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+function Footer() {
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <footer>
+            <p>Copyright Pixell River Financial {currentYear}.</p>
+        </footer>
+    );
+}
+
+export default Footer;
